96328fdfa9363d5978c9ce42c4ea8589e213f684
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-07-14 22:15:39 +0300

    added question card

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 import classes from './Quiz.module.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {
+                answers:[
+                    {text: 'Answer 1'},
+                    {text: 'Answer 2'},
+                    {text: 'Answer 3'},
+                    {text: 'Answer 4'}
+                ]
+            }
+        ]
     }
 
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+                <div className={classes.QuizWrapper}>
+                    <h1>Answer all questions</h1>
+                    <ActiveQuiz
+                        answers={this.state.quiz[0].answers}
+                    />
+                </div>
             </div>
         )
 
