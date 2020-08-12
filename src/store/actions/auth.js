e046ea77e2a03a654b57e84235f00ac38865ab7d
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-08-12 22:10:07 +0300

    menu changed and autologin/out added

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -18,7 +18,7 @@ export function auth(email, password, isLogin) {
         const response = await axios.post(url, authData)
         const data = response.data
 
-        const expirationDate = new Date(new Date().getTime + data.expiresIn * 1000)
+        const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
 
         localStorage.setItem('token', data.idToken)
         localStorage.setItem('userId', data.localId)
@@ -43,6 +43,23 @@ export function logout() {
     localStorage.removeItem('expirationDate')
     return {
         type: AUTH_LOGOUT
+    }
+}
+
+export function autoLogin() {
+    return dispatch => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            dispatch(logout())
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            if (expirationDate <= new Date()) {
+                dispatch(logout())
+            } else {
+                dispatch(authSuccess(token))
+                dispatch(autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000))
+            }
+        }
     }
 }
 
